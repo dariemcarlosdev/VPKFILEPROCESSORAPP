ffe315303b1144673fea32edecdfef54e9f9bcfe
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-10-09 11:31:58 -0400

    ### Summary of Most Important Changes: A new namespace `VPKFILEPROCESSOR.FILEMANAGEMENTSERVICE.Models` has been introduced, which includes a new internal class `FileUploadModel`. This class contains two required string properties: `FileName` and `FileUrl`, both with public getters and setters.

---
+++ VPKFILEPROCESSOR.FILEMANAGEMENTSERVICE/Models/FileUploadModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VPKFILEPROCESSOR.FILEMANAGEMENTSERVICE.Models
+{
+    internal class FileUploadModel
+    {
+        public required string FileName { get; set; }
+        public required string FileUrl { get; set; }
+    }
+}
