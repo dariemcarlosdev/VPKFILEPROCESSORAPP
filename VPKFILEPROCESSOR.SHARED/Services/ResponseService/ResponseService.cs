f3c1c23234cd6a47dad17125a453413694aa70ea
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-11-05 10:13:58 -0500

    ### Summary of Important Changes Two new classes, `FileResponse` and `ResponseService<T>`, were introduced in the `VPKFILEPROCESSOR.SHARED.Services.ResponseService` namespace to handle file processing responses. Additionally, the `<Folder Include="Services\" />` entry was removed from the `VPKFILEPROCESSOR.SHARED.csproj` file.
    ### List of Changes
    1. **Addition of `FileResponse` Class**:
       - A new class named `FileResponse` was added to the `VPKFILEPROCESSOR.SHARED.Services.ResponseService` namespace.
       - Purpose: To return the file name and file URL after processing a file.
       - Reference: `VPKFILEPROCESSOR.SHARED.Services.ResponseService.FileResponse`
    
    2. **Addition of `ResponseService<T>` Class**:
       - A new generic class named `ResponseService<T>` was added to the `VPKFILEPROCESSOR.SHARED.Services.ResponseService` namespace.
       - Purpose: To return response data after processing a file, including the data, a message, and a success flag.
       - Reference: `VPKFILEPROCESSOR.SHARED.Services.ResponseService.ResponseService<T>`
    
    3. **Removal of `<Folder Include="Services\" />` Entry**:
       - The `<Folder Include="Services\" />` entry was removed from the `VPKFILEPROCESSOR.SHARED.csproj` file.
       - Reference: `VPKFILEPROCESSOR.SHARED.csproj`

---
+++ VPKFILEPROCESSOR.SHARED/Services/ResponseService/ResponseService.cs
@@ -0,0 +1,16 @@
+﻿namespace VPKFILEPROCESSOR.SHARED.Services.ResponseService
+{
+    /// <summary>
+    /// Response Service class is created to return the response data after processing the file.It a generic class that can be used to return any type of data.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="data"></param>
+    /// <param name="message"></param>
+    /// <param name="isSuccess"></param>
+    public class ResponseService<T>(T data, string message, bool isSuccess)
+    {
+        public T? Data { get; set; } = data;
+        public string Message { get; set; } = message;
+        public bool IsSuccess { get; set; } = isSuccess;
+    }
+}
